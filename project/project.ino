a66051cb5c478076e44fc95bad6268436a828eb2
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-06-06 03:25:37 +0900

    final

--- project/project.ino
+++ project/project.ino
@@ -6,6 +6,7 @@
 
 SoftwareSerial BTSerial(10, 11); // HC-06 모듈의 RX, TX 핀을 10번, 11번으로 설정
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN_LED, NEO_GRB + NEO_KHZ800);
+int delayval = 100; // 지연 시간
 int pattern = 0;
 bool patternRunning = false; // 패턴 실행 여부를 저장하는 변수
 
@@ -79,26 +80,51 @@ void changePattern(int pattern) {
       theaterChaseRainbow(80); // 무지개 패턴 2
       turnOffLEDs();
       break;
-    // 2, 3, 4 케이스문은 일부로 break 빼서 연속적으로 실행하도록 함
+
     case 2:
       for (int j = 0; j < 5; j++) {
         colorWipe(pixels.Color(255, 0, 0), 20); // 빨간색 패턴(RGB 색상 패턴)
         turnOffLEDs();
       }
+      break;
       
     case 3:
       for (int j = 0; j < 5; j++) {
         colorWipe(pixels.Color(0, 0, 255), 20); // 파란색 패턴(RGB 색상 패턴)
         turnOffLEDs();
       }
+      break;
       
     case 4:
       for (int j = 0; j < 5; j++) {
         colorWipe(pixels.Color(0, 255, 0), 20); // 초록색 패턴(RGB 색상 패턴)
         turnOffLEDs();
       }
-      
+      break;
+    
+    case 5:
+      alternateColor(); // 빨강과 초록이 섞여있는 패턴 
+      break;
+
+    case 6:
+    for (int j = 0; j < 5; j++) { // 빨,초,파 3개의 LED만 켜지는 패턴
+      setPixelColors();
+    }
+      break;
+
+    case 7:
+    for (int j = 0; j < 5; j++) { // 여러가지 LED가 한번에 켜지는 패턴
+      setPixelColors2();
+    }
+      break;
+    
+    case 8:
+    for (int j = 0; j < 5; j++) { // 자연스럽게 꺼지고 켜지는 Fadein 패턴
+      fadeRainbow(250);
+    }
+    break;
   }
+  
 }
 
 
@@ -169,4 +195,128 @@ uint32_t Wheel(byte WheelPos) {
   }
   WheelPos -= 170;
   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void alternateColor() {
+  for (int count = 0; count < 5; count++) {
+    for (int i = 0; i < NUM_LEDS; i += 2) {
+      pixels.setPixelColor(i, pixels.Color(255, 0, 0));  // 빨간색 LED
+    }
+
+    for (int i = 1; i < NUM_LEDS; i += 2) {
+      pixels.setPixelColor(i, pixels.Color(0, 255, 0));  // 초록색 LED
+    }
+  
+  pixels.show();
+  delay(500);
+
+  pixels.clear();  // 모든 LED를 끔
+  pixels.show();
+  delay(500);
+  }
+}
+
+void setPixelColors() {
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0));    // 첫 번째 픽셀: 빨강
+  pixels.setPixelColor(1, pixels.Color(0, 255, 0));    // 두 번째 픽셀: 초록
+  pixels.setPixelColor(2, pixels.Color(0, 0, 255));    // 세 번째 픽셀: 파랑
+  // 나머지 픽셀도 필요한 만큼 설정
+
+  pixels.show();  // 설정한 색으로 픽셀 표시
+
+  delay(1000);  // 1초 동안 유지
+
+  // 모든 픽셀 끄기
+  for (int i = 0; i < NUM_LEDS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+  }
+
+  pixels.show();  // 픽셀 끄기 적용
+
+  delay(1000);  // 1초 동안 유지
+}
+
+void setPixelColors2() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (i % 3 == 0) {
+      pixels.setPixelColor(i, pixels.Color(255, 0, 0));    // 빨강
+    } else if (i % 3 == 1) {
+      pixels.setPixelColor(i, pixels.Color(0, 255, 0));    // 초록
+    } else {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 255));    // 파랑
+    }
+  }
+
+  pixels.show();    // 설정한 색으로 픽셀 표시
+
+  delay(1000);      // 1초 동안 유지
+
+  // 모든 픽셀 끄기
+  for (int i = 0; i < NUM_LEDS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+  }
+
+  pixels.show();    // 픽셀 끄기 적용
+
+  delay(1000);      // 1초 동안 유지
+}
+
+
+int fadeAmount = 100;  // 각 단계에서 페이드인/페이드아웃되는 양
+
+void fadeRainbow(int wait) {
+  // 무지개 색상 초기화
+  for (int i = 0; i < pixels.numPixels(); i++) {
+    uint32_t color = Wheel((i * 256 / pixels.numPixels()) & 255);
+    pixels.setPixelColor(i, color);
+  }
+
+  pixels.show();  // 초기 무지개 색상 표시
+  delay(wait);    // 지연 시간
+
+  for (int k = 0; k < 2; k++) {  // 전체 픽셀 페이드인/페이드아웃을 2번 반복
+    // 페이드아웃
+    for (int brightness = 255; brightness >= 0; brightness -= fadeAmount) {
+      for (int i = 0; i < pixels.numPixels(); i++) {
+        // 현재 픽셀의 색상 값을 가져와 밝기를 조절
+        uint32_t color = pixels.getPixelColor(i);
+        uint8_t r = (color >> 16) & 0xFF;
+        uint8_t g = (color >> 8) & 0xFF;
+        uint8_t b = color & 0xFF;
+        r = (r * brightness) / 255;
+        g = (g * brightness) / 255;
+        b = (b * brightness) / 255;
+        pixels.setPixelColor(i, pixels.Color(r, g, b));
+      }
+
+      pixels.show();  // 픽셀 색상 업데이트
+      delay(wait);    // 지연 시간
+    }
+
+    // 페이드인
+    for (int brightness = 0; brightness <= 255; brightness += fadeAmount) {
+      for (int i = 0; i < pixels.numPixels(); i++) {
+        // 현재 픽셀의 색상 값을 가져와 밝기를 조절
+        uint32_t color = pixels.getPixelColor(i);
+        uint8_t r = (color >> 16) & 0xFF;
+        uint8_t g = (color >> 8) & 0xFF;
+        uint8_t b = color & 0xFF;
+        r = (r * brightness) / 255;
+        g = (g * brightness) / 255;
+        b = (b * brightness) / 255;
+        pixels.setPixelColor(i, pixels.Color(r, g, b));
+      }
+
+      pixels.show();  // 픽셀 색상 업데이트
+      delay(wait);    // 지연 시간
+    }
+  }
+
+  // 모든 픽셀 끄기
+  for (int i = 0; i < pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+  }
+
+  pixels.show();  // 픽셀 끄기 적용
+  delay(wait);    // 지연 시간
 }
